516846ad1af4f5072709e5cd9af8d364f4c5b9c0
KumarHarsh4041 <kr.harsh4125@gmail.com>
2025-08-09 19:57:58 +0530

    biulding

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const http= require("http");
+const path = require("path");
+
+const socketio= require("socket.io")
+const server = http.createServer(app);
+
+const io=socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket){
+  console.log("connected")
+})
 
 app.get("/", function(req, res) {
-  res.send("Hello, World");
+  res.render("index");
 });
-app.listen(3000);
+server.listen(3000);
